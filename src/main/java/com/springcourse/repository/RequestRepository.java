0ce90fb746c44968f292cf6c47903ff19af4f843
Vicente Silva <vicente.pspenha@gmail.com>
2019-05-05 14:52:08 -0300

    Criação do repositório da entidade Request.

---
+++ src/main/java/com/springcourse/repository/RequestRepository.java
@@ -0,0 +1,19 @@
+package com.springcourse.repository;
+
+import com.springcourse.domain.Request;
+import com.springcourse.domain.enums.RequestState;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RequestRepository extends JpaRepository<Request, Long> {
+
+    List<Request> findAllByOwnerId(Long id);
+
+    @Query("UPDATE Request SET state = ?2 WHERE id = ?1")
+    Request updateStatus(Long id, RequestState requestState);
+
+}
