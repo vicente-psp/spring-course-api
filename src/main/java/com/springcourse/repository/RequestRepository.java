25f7457fb1cffc1dd2a70dc66d3047eadf6ba243
Vicente Silva <vicente.pspenha@gmail.com>
2019-05-05 22:28:07 -0300

    Alterado propriedade state para request_state, alterado tipo de retorno de Request para Long e adicionado anotações Transaction e Modifying.

--- src/main/java/com/springcourse/repository/RequestRepository.java
+++ src/main/java/com/springcourse/repository/RequestRepository.java
@@ -3,8 +3,10 @@ package com.springcourse.repository;
 import com.springcourse.domain.Request;
 import com.springcourse.domain.enums.RequestState;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,7 +15,9 @@ public interface RequestRepository extends JpaRepository<Request, Long> {
 
     List<Request> findAllByOwnerId(Long id);
 
-    @Query("UPDATE tb_request SET state = ?2 WHERE id = ?1")
-    Request updateStatus(Long id, RequestState requestState);
+    @Transactional(readOnly = false)
+    @Modifying
+    @Query("UPDATE tb_request SET request_state = ?2 WHERE id = ?1")
+    Long updateStatus(Long id, RequestState requestState);
 
 }
