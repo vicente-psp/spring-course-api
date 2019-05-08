04d83e1520400b4d365dc48af31911bd70b52bbb
Vicente Silva <vicente.pspenha@gmail.com>
2019-05-07 21:30:54 -0300

    Criado controlador RequestResource para recurso Request.

---
+++ src/main/java/com/springcourse/resource/RequestResource.java
@@ -0,0 +1,43 @@
+package com.springcourse.resource;
+
+import com.springcourse.domain.Request;
+import com.springcourse.service.RequestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(name = "requests")
+public class RequestResource {
+
+    @Autowired private RequestService requestService;
+
+    @PostMapping
+    public ResponseEntity<Request> save(@RequestBody Request request) {
+        Request requestCreated = requestService.save(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(requestCreated);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Request> update(@PathVariable Long id, @RequestBody Request request) {
+        request.setId(id);
+        Request requestUpdated = requestService.save(request);
+        return ResponseEntity.ok(requestUpdated);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Request> getById(@PathVariable Long id) {
+        Request request = requestService.getById(id);
+        return ResponseEntity.ok(request);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Request>> listAll() {
+        List<Request> requests = requestService.listAll();
+        return ResponseEntity.ok(requests);
+    }
+
+}
