1c759a0190d0e4a4fe667ccfc403fe1cc8221d55
Vicente Silva <vicente.pspenha@gmail.com>
2019-05-08 22:34:50 -0300

    Configuração de carregamento de página sob demanda - criação dos métodos listAllOnLazyMode e listAllByOwnerIdOnLazyMode.

--- src/main/java/com/springcourse/service/RequestService.java
+++ src/main/java/com/springcourse/service/RequestService.java
@@ -3,8 +3,13 @@ package com.springcourse.service;
 import com.springcourse.domain.Request;
 import com.springcourse.domain.enums.RequestState;
 import com.springcourse.exception.NotFoundException;
+import com.springcourse.model.PageModel;
+import com.springcourse.model.PageRequestModel;
 import com.springcourse.repository.RequestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -38,9 +43,28 @@ public class RequestService {
         return requests;
     }
 
+    public PageModel<Request> listAllOnLazyMode(PageRequestModel pageRequestModel) {
+        Pageable pageable = PageRequest.of(pageRequestModel.getPage(), pageRequestModel.getSize());
+        Page<Request> page = requestRepository.findAll(pageable);
+
+        PageModel<Request> pageModel = new PageModel<>((int) page.getTotalElements(), page.getSize(), page.getTotalPages(), page.getContent());
+
+        return pageModel;
+    }
+
     public List<Request> listAllByOwnerId(Long ownerId) {
         List<Request> requests = requestRepository.findAllByOwnerId(ownerId);
         return requests;
+    }
+
+    public PageModel<Request> listAllByOwnerIdOnLazyMode(Long ownerId, PageRequestModel pageRequestModel) {
+        Pageable pageable = PageRequest.of(pageRequestModel.getPage(), pageRequestModel.getSize());
+
+        Page<Request> page = requestRepository.findAllByOwnerId(ownerId, pageable);
+
+        PageModel<Request> pageModel = new PageModel<>((int) page.getTotalElements(), page.getSize(), page.getTotalPages(), page.getContent());
+
+        return pageModel;
     }
 
 }
