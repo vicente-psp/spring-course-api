c1e9078455ab8826457c6dbad8776860b8cc7f52
Vicente Silva <vicente.pspenha@gmail.com>
2019-05-10 22:56:07 -0300

    Adicionado validação para os atributos.

--- src/main/java/com/springcourse/dto/UserLoginDto.java
+++ src/main/java/com/springcourse/dto/UserLoginDto.java
@@ -5,12 +5,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter @Setter
 public class UserLoginDto {
 
+    @Email(message = "Endereço de email inválido")
     private String email;
+
+    @NotBlank(message = "Password é obrigatório.")
     private String password;
 
 }
