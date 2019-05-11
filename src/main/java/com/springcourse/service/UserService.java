b1eb310811769a04227f75d6098ed84b8658eabf
Vicente Silva <vicente.pspenha@gmail.com>
2019-05-11 17:53:44 -0300

    Removido imports não utilizados.

--- src/main/java/com/springcourse/service/UserService.java
+++ src/main/java/com/springcourse/service/UserService.java
@@ -12,8 +12,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
-//import java.awt.print.Pageable;
 import java.util.List;
 import java.util.Optional;
 
