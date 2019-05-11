a9e886882eb6edb7b551c75a2131b92ac7b4029a
Vicente Silva <vicente.pspenha@gmail.com>
2019-05-11 18:54:46 -0300

    Alterado a entidade que recebe o corpo da requisição e incluído validação no método save.

--- src/main/java/com/springcourse/resource/RequestStageResource.java
+++ src/main/java/com/springcourse/resource/RequestStageResource.java
@@ -1,11 +1,14 @@
 package com.springcourse.resource;
 
 import com.springcourse.domain.RequestStage;
+import com.springcourse.dto.RequestStageSaveDto;
 import com.springcourse.service.RequestStageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping(value = "request-stages")
@@ -14,7 +17,8 @@ public class RequestStageResource {
     @Autowired private RequestStageService requestStageService;
 
     @PostMapping
-    public ResponseEntity<RequestStage> save(RequestStage requestStage) {
+    public ResponseEntity<RequestStage> save(@RequestBody @Valid RequestStageSaveDto requestStageSaveDto) {
+        RequestStage requestStage = requestStageSaveDto.transformToRequestStage();
         RequestStage requestStageCreated = requestStageService.save(requestStage);
         return ResponseEntity.status(HttpStatus.CREATED).body(requestStageCreated);
     }
