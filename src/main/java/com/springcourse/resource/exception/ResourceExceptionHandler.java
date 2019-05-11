ba4276b1877fdf5baf5dbf141cdd55857bb34968
Vicente Silva <vicente.pspenha@gmail.com>
2019-05-10 22:55:37 -0300

    Adicionado método para tratar mensagem de erro BAD_REQUEST.

--- src/main/java/com/springcourse/resource/exception/ResourceExceptionHandler.java
+++ src/main/java/com/springcourse/resource/exception/ResourceExceptionHandler.java
@@ -1,20 +1,33 @@
 package com.springcourse.resource.exception;
 
 import com.springcourse.exception.NotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
 
 @ControllerAdvice
-public class ResourceExceptionHandler {
+public class ResourceExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler
     public ResponseEntity<ApiError> handlerNotFoundException(NotFoundException e) {
         ApiError apiError = new ApiError(HttpStatus.NOT_FOUND.value(), e.getMessage(), new Date());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiError);
+    }
+
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders httpHeaders, HttpStatus httpStatus, WebRequest webRequest) {
+        String defaultMessage = ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+
+        ApiError error = new ApiError(HttpStatus.BAD_REQUEST.value(), defaultMessage, new Date());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
 }
