db8927d9f5fab1ae6fc1e726c60f22c69552dbe6
Vicente Silva <vicente.pspenha@gmail.com>
2019-05-11 17:48:34 -0300

    Criado método updateState, alterado nos métodos save e update os tipos de request e inserido validações.

--- src/main/java/com/springcourse/resource/RequestResource.java
+++ src/main/java/com/springcourse/resource/RequestResource.java
@@ -2,6 +2,9 @@ package com.springcourse.resource;
 
 import com.springcourse.domain.Request;
 import com.springcourse.domain.RequestStage;
+import com.springcourse.dto.RequestSaveDto;
+import com.springcourse.dto.RequestUpdateDto;
+import com.springcourse.dto.RequestUpdateStateDto;
 import com.springcourse.model.PageModel;
 import com.springcourse.model.PageRequestModel;
 import com.springcourse.service.RequestService;
@@ -11,7 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping(value = "requests")
@@ -21,15 +24,17 @@ public class RequestResource {
     @Autowired private RequestStageService requestStageService;
 
     @PostMapping
-    public ResponseEntity<Request> save(@RequestBody Request request) {
+    public ResponseEntity<Request> save(@RequestBody @Valid RequestSaveDto requestSaveDto) {
+        Request request = requestSaveDto.transformToRequest();
         Request requestCreated = requestService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(requestCreated);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Request> update(@PathVariable Long id, @RequestBody Request request) {
+    public ResponseEntity<Request> update(@PathVariable Long id, @RequestBody @Valid RequestUpdateDto requestUpdateDto) {
+        Request request = requestUpdateDto.transformToRequest();
         request.setId(id);
-        Request requestUpdated = requestService.save(request);
+        Request requestUpdated = requestService.update(request);
         return ResponseEntity.ok(requestUpdated);
     }
 
@@ -56,6 +61,17 @@ public class RequestResource {
         PageModel<RequestStage> pageModel = requestStageService.listAllByRequestIdOnLazyMode(id, pageRequestModel);
 
         return ResponseEntity.ok(pageModel);
+    }
+
+    @PatchMapping("/state/{id}")
+    public ResponseEntity<?> updateState(@RequestBody @Valid RequestUpdateStateDto requestUpdateStateDto, @PathVariable Long id) {
+        Request request = new Request();
+        request.setId(id);
+        request.setState(requestUpdateStateDto.getState());
+
+        requestService.updateRole(request);
+
+        return ResponseEntity.ok().build();
     }
 
 }
