04481d8dfddd72b6b1e854f5170fcee07e74f498
Vicente Silva <vicente.pspenha@gmail.com>
2019-06-23 22:12:31 -0300

    Criado classe estendentido WebSecurityConfigurerAdapter e sobrescrito método configure com parâmetro AuthenticationManagerBuilder.

---
+++ src/main/java/com/springcourse/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.springcourse.security;
+
+import com.springcourse.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired private UserService userService;
+    @Autowired private CustomPasswordEncoder passwordEncoder;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder);
+    }
+}
