2ab41c6727804bdea970bca0300ebce44c15e227
Vicente Silva <vicente.pspenha@gmail.com>
2019-06-24 22:51:43 -0300

    Autenticado User com métodos do spring.

--- src/main/java/com/springcourse/resource/UserResource.java
+++ src/main/java/com/springcourse/resource/UserResource.java
@@ -13,6 +13,10 @@ import com.springcourse.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -23,6 +27,7 @@ public class UserResource {
 
     @Autowired private UserService userService;
     @Autowired private RequestService requestService;
+    @Autowired private AuthenticationManager authenticationManager;
 
     @PostMapping
     public ResponseEntity<User> save(@RequestBody @Valid UserSaveDto userSaveDto) {
@@ -56,8 +61,13 @@ public class UserResource {
 
     @PostMapping("/login")
     public ResponseEntity<User> login(@RequestBody @Valid UserLoginDto userLoginDto) {
-        User loggedUser = userService.login(userLoginDto.getEmail(), userLoginDto.getPassword());
-        return ResponseEntity.ok(loggedUser);
+        UsernamePasswordAuthenticationToken token =
+                new UsernamePasswordAuthenticationToken(userLoginDto.getEmail(), userLoginDto.getPassword());
+        Authentication authentication = authenticationManager.authenticate(token);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return ResponseEntity.ok(null);
     }
 
     @GetMapping("/{id}/requests")
